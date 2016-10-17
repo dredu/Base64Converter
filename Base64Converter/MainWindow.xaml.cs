348df1a21d9b84bbad0410684ee2fe988e710e8c
dzwiedziu <dzwiedziu66@gmail.com>
2016-10-18 01:34:19 +0200

    need to fix from Base64 to file

---
+++ Base64Converter/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Windows;
+
+namespace Base64Converter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void FromFileToBase64(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            if (openFile.ShowDialog() == true)
+            {
+                try
+                {
+                    var parseTo64 = Convert.ToBase64String(File.ReadAllBytes(openFile.FileName));
+                    StreamWriter file = new StreamWriter("c:\\test.txt");
+                    file.WriteLine(parseTo64);
+                    file.Close();
+                    MessageBox.Show("Encoding was successful");
+                }
+                catch (FileLoadException ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+        }
+
+        private void FromBase64ToFile(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            if (openFile.ShowDialog() == true)
+            {
+                try
+                {
+                    //@Todo poprawka
+                    var parseToFile = Convert.FromBase64String(File.ReadAllText(openFile.FileName));
+                    FileStream stream = null;
+                    using (var writer = new BinaryWriter(stream))
+                    {
+                        writer.Write(parseToFile);
+                    }
+                    File.WriteAllBytes("c:\\" + stream.Name, parseToFile);
+                    MessageBox.Show("Encoding was successful");
+                }
+                catch (FileLoadException ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+        }
+    }
+}
